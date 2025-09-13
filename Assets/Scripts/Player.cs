f4597acffb90c9c451e14622b673b8b891f667ee
sohtatomisato <sohta.tommy@gmail.com>
2025-09-13 17:02:17 +0900

    プレイヤーのWASD移動を追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 毎秒の移動速度指定
+    [SerializeField]
+    private float moveSpeed = 1;
+
+    [SerializeField]
+    private bool isEnableSense = true;
+
+    // 移動ベクトル保持用
+    private Vector2 moveInput;
+
+    new private Rigidbody rigidbody;
+    public bool IsSleeping { get; private set; }
+
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Sleep()
     {
-        
+        IsSleeping = true;
+    }
+    public void WakeUp()
+    {
+        IsSleeping = false;
+    }
+
+    // PlayerInputからUnityEventで起動
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsSleeping) return;
+
+        if (moveInput != Vector2.zero)
+        {
+            Move(moveInput);
+
+            transform.rotation = Quaternion.Euler(0, 
+                (Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg), 0);
+
+            // 以下、Unityの機能を使った簡単バージョン(AI頼り)
+            //transform.rotation = Quaternion.LookRotation(new Vector3(moveInput.x, 0f, moveInput.y));
+        }
+        else if (!isEnableSense)
+        {
+            Move(moveInput);
+        }
+    }
+
+    private void Move(Vector2 Input)
+    {
+        Vector3 velocity = rigidbody.linearVelocity;
+        velocity.x = Input.x * moveSpeed;
+        velocity.z = Input.y * moveSpeed;
+        rigidbody.linearVelocity = velocity;
     }
 }
