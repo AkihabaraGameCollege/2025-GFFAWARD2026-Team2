f34409f33e044d78fcf99a98a381babe3f82dac1
sohtatomisato <sohta.tommy@gmail.com>
2025-09-21 03:37:43 +0900

    プレイヤーにジャンプを実装

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
@@ -9,13 +8,39 @@ public class Player : MonoBehaviour
     private float moveSpeed = 1;
 
     [SerializeField]
+    private float jumpForce = 10;
+
+    [SerializeField]
     private bool isEnableSense = true;
+
+    [SerializeField]
+    [Tooltip("ジャンプに必要な速度を指定")]
+    private float requiredJumpSpeed = 0.1f;
+
+
+    [Header("GroundChecker")]
+    [SerializeField]
+    private Vector3 groundCheckStartPoint = new Vector3(0, -0.5f, 0);
+    [SerializeField]
+    private Vector3 groundCheckEndPoint = new Vector3(0, -1.5f, 0);
+
+    private bool IsGrounded => Physics.Linecast(transform.position + groundCheckStartPoint, transform.position + groundCheckEndPoint);
 
     // 移動ベクトル保持用
     private Vector2 moveInput;
 
     new private Rigidbody rigidbody;
     public bool IsSleeping { get; private set; }
+
+    enum MotionState
+    {
+        Stopping,
+        Walking,
+        JumpAnticipation,
+        Jumping,
+    }
+
+    MotionState motionState = MotionState.Stopping;
 
     private void Start()
     {
@@ -37,31 +62,102 @@ public class Player : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!IsSleeping) Jump(jumpForce);
+    }
+
     private void FixedUpdate()
     {
         if (IsSleeping) return;
 
-        if (moveInput != Vector2.zero)
+        switch (motionState)
         {
-            Move(moveInput);
+            case MotionState.Stopping:
+                if (moveInput != Vector2.zero)
+                {
+                    motionState = MotionState.Walking;
 
-            transform.rotation = Quaternion.Euler(0, 
-                (Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg), 0);
+                    Move(moveInput);
+                }
+                break;
+            case MotionState.Walking:
+                if (moveInput != Vector2.zero)
+                {
+                    Move(moveInput);
+                }
+                else
+                {
+                    motionState = MotionState.Stopping;
 
-            // 以下、Unityの機能を使った簡単バージョン(AI頼り)
-            //transform.rotation = Quaternion.LookRotation(new Vector3(moveInput.x, 0f, moveInput.y));
-        }
-        else if (!isEnableSense)
-        {
-            Move(moveInput);
+                    if (!isEnableSense)
+                    {
+                        Move(moveInput);
+                    }
+                }
+                break;
+            case MotionState.JumpAnticipation:
+                if (moveInput != Vector2.zero)
+                {
+                    Move(moveInput);
+                }
+                if (!IsGrounded)
+                {
+                    motionState = MotionState.Jumping;
+                }
+                else if (rigidbody.linearVelocity.y < requiredJumpSpeed)
+                {
+                    if (moveInput != Vector2.zero)
+                    {
+                        motionState = MotionState.Walking;
+                    }
+                    else
+                    {
+                        motionState = MotionState.Stopping;
+                    }
+                }
+                break;
+            case MotionState.Jumping:
+                if (moveInput != Vector2.zero)
+                {
+                    Move(moveInput);
+                }
+                if (IsGrounded)
+                {
+                    if (moveInput != Vector2.zero)
+                    {
+                        motionState = MotionState.Walking;
+                    }
+                    else
+                    {
+                        motionState = MotionState.Stopping;
+                    }
+                }
+                break;
         }
     }
 
     private void Move(Vector2 Input)
     {
+        // 移動(速度変更)
         Vector3 velocity = rigidbody.linearVelocity;
         velocity.x = Input.x * moveSpeed;
         velocity.z = Input.y * moveSpeed;
         rigidbody.linearVelocity = velocity;
+
+        // 回転
+        transform.rotation = Quaternion.Euler(0,
+                        (Mathf.Atan2(Input.x, Input.y) * Mathf.Rad2Deg), 0);
+        // 以下、Unityの機能を使った簡単バージョン(AI頼り)
+        //transform.rotation = Quaternion.LookRotation(new Vector3(moveInput.x, 0f, moveInput.y));
+    }
+
+    private void Jump(float power)
+    {
+        Vector3 velocity = rigidbody.linearVelocity;
+        velocity.y = power;
+        rigidbody.linearVelocity = velocity;
+
+        motionState = MotionState.JumpAnticipation;
     }
 }
