7da85f7dd364086cf03ac5e362202bd20c02d947
sohtatomisato <sohta.tommy@gmail.com>
2025-10-16 13:22:14 +0900

    [update] プレイヤーの攻撃を簡単なものに変更

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,6 @@ public class Player : MonoBehaviour
     private float rotationOffset = 0;
 
     [SerializeField]
-    private bool isEnableSense = true;
-
-    [SerializeField]
     [Tooltip("ジャンプに必要な速度を指定")]
     private float requiredJumpSpeed = 0.1f;
 
@@ -31,16 +28,7 @@ public class Player : MonoBehaviour
     [Header("攻撃関連")]
     [SerializeField]
     [Tooltip("childを指定")]
-    private AttackCollider attackCollider = null;
-    [SerializeField]
-    [Tooltip("攻撃判定の一番上の角度(度数法)を指定")]
-    private float attackStartDegree = 0;
-    [SerializeField]
-    [Tooltip("攻撃判定の一番下の角度(度数法)を指定")]
-    private float totalMoveDegree = 0;
-    [SerializeField]
-    [Tooltip("一秒で動かす各度(度数法)を指定")]
-    private float attackRotationSpeed = 1;
+    private GameObject attackCollider = null;
 
     // ポーズUIを指定します。
     [SerializeField]
@@ -67,6 +55,8 @@ public class Player : MonoBehaviour
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+
+        attackCollider.SetActive(false);
     }
 
     public void Sleep()
@@ -122,15 +112,6 @@ public class Player : MonoBehaviour
                 if (moveInput != Vector2.zero)
                 {
                     Move(moveInput);
-                }
-                else
-                {
-                    motionState = MotionState.Stopping;
-
-                    if (!isEnableSense)
-                    {
-                        Move(moveInput);
-                    }
                 }
                 break;
             case MotionState.JumpAnticipation:
@@ -200,7 +181,16 @@ public class Player : MonoBehaviour
 
     private void Attack()
     {
-        attackCollider.Init(attackStartDegree, totalMoveDegree, attackRotationSpeed);
+        attackCollider.SetActive(true);
+        StartCoroutine(AttackTimer());
+
         Debug.Log("Attack");
+    }
+
+    IEnumerator AttackTimer()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        attackCollider.SetActive(false);
     }
 }
