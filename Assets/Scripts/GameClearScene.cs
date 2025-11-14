528ad2eabd78c7d0eff0d642faa24186ed0d1df5
sohtatomisato <sohta.tommy@gmail.com>
2025-11-14 15:20:27 +0900

    [update] gameclear2 music ready 変更内容： ・gameclear2のBGMを再生できるように(音声ファイル自体の追加はまだ)

--- Assets/Scripts/GameClearScene.cs
+++ Assets/Scripts/GameClearScene.cs
@@ -12,13 +12,17 @@ public class GameClearScene : MonoBehaviour
     private float loadWaitTime = 1;
 
     [SerializeField]
+    private float musicWaitTime = 24;
+
+    [SerializeField]
     private Button nextButton = null;
 
     private bool isLoadable = false;
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.None;// カーソルのロックを解除（中山が編集）
+        AudioPlayer.instance.PlayBGM(7); // Gameclear1を再生（富里が編集）
+        Cursor.lockState = CursorLockMode.None;// カーソルのロックを解除（富里が編集）
         StartCoroutine(OnStart());
     }
 
@@ -28,6 +32,8 @@ public class GameClearScene : MonoBehaviour
         isLoadable = true;
         // button select
         nextButton.Select();
+        yield return new WaitForSeconds(musicWaitTime - loadWaitTime);
+        AudioPlayer.instance.PlayBGM(16);// gameclear2を再生（富里が編集）
     }
 
     public void OnTitleButtonClick()
