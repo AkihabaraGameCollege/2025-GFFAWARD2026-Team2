0b47c6dff0ba5dd922e6b4dc7c733368e1ab628b
sohtatomisato <sohta.tommy@gmail.com>
2025-12-03 22:03:25 +0900

    [update] すでに倒したボスのもとへは行けないように

--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -14,28 +14,55 @@ public class StageSelect : MonoBehaviour
     [SerializeField]
     private string nextSceneName3;
 
+    private bool isDefeatedBoss1 = false;
+    private bool isDefeatedBoss2 = false;
+    private bool isDefeatedBoss3 = false;
 
     // 登録・音楽再生用（中山が編集）
     void Awake()
     {
         AudioPlayer.instance.PlayBGM(13); // stageSelectMusicを再生(中山が編集)
+
+        if (PlayerPrefs.GetInt("AttackLevel",1) == 2)
+        {
+            isDefeatedBoss1 = true;
+        }
+
+        if (PlayerPrefs.GetInt("JumpLevel",1) == 2)
+        {
+            isDefeatedBoss2 = true;
+        }
+
+        if (PlayerPrefs.GetInt("SpeedLevel",1) == 2)
+        {
+            isDefeatedBoss3 = true;
+        }
     }
 
     // ボス戦1へ行くボタンが押されたときに呼び出されるメソッド（中山が編集）
     public void PressBoss1Button()
     {
-        SceneManager.LoadScene(nextSceneName1);// 次のシーンへ遷移（中山が編集）
+        if (!isDefeatedBoss1)
+        {
+            SceneManager.LoadScene(nextSceneName1);// 次のシーンへ遷移（中山が編集）
+        }
     }
 
     // ボス戦2へ行くボタンが押されたときに呼び出されるメソッド（中山が編集）
     public void PressBoss2Button()
     {
-        SceneManager.LoadScene(nextSceneName2);// 次のシーンへ遷移（中山が編集）
+        if (!isDefeatedBoss2)
+        {
+            SceneManager.LoadScene(nextSceneName2);// 次のシーンへ遷移（中山が編集）
+        }
     }
 
     // ボス戦3へ行くボタンが押されたときに呼び出されるメソッド（中山が編集）
     public void PressBoss3Button()
     {
-        SceneManager.LoadScene(nextSceneName3);// 次のシーンへ遷移（中山が編集）
+        if (!isDefeatedBoss3)
+        {
+            SceneManager.LoadScene(nextSceneName3);// 次のシーンへ遷移（中山が編集）
+        }
     }
 }
